9d58a494353bb9b6ab4ae705c112ad4e0f3d5d0e
matthewmjm <matthewmjm@yahoo.com>
2016-09-01 22:51:02 -0600

    Basic HMTL CSS and JS structure

---
+++ js/app.js
@@ -0,0 +1,100 @@
+/*--- Step 1 - Defining global variables ---*/
+var questionsArray = [
+    //Question 1
+    {
+        questionText: 'What is the most expensive car in the world?',
+        questionChoices: ['Bugatti Veyron', 'Ferrari 250 gto', 'Rolls Royce Phantom'],
+        questionCorrectChoice: 1,
+        questionImage: 'policeman.jpg',
+        correctDetails: '1962 Ferrari 250 GTO is $52 000 000. '
+    },
+
+    //Question 2
+    {
+        questionText: 'What is the smallest car in the world?',
+        questionChoices: ['Peel P50', 'BMW Mini', 'Mercedes Smart'],
+        questionCorrectChoice: 0,
+        questionImage: 'container-ship.jpg',
+        correctDetails: 'The Peel P50 is 54 in (1,372 mm) long and 39 in (991 mm) wide.'
+    },
+
+    //Question 3
+    {
+        questionText: 'How much horsepower does a monster truck have?',
+        questionChoices: ['Around 2000 bhp', 'Around 3000 bhp', 'Around 4000 bhp'],
+        questionCorrectChoice: 0,
+        questionImage: 'policeman.jpg',
+        correctDetails: 'The Grave Digger has 1500-2000 bhp.'
+    },
+
+    //Question 4
+    {
+        questionText: 'How fast is the fastest land speed record for cars?',
+        questionChoices: ['593 mph', '843 mph', '763 mph'],
+        questionCorrectChoice: 2,
+        questionImage: 'policeman.jpg',
+        correctDetails: 'The official land-speed record (measured over one mile) is 1,227.985 km/h (763.035 mi/h) by Thrust SSC.'
+    },
+
+    //Question 5
+    {
+        questionText: 'How many car companies are there in the world?',
+        questionChoices: ['Around 1500', 'Around 2500', 'Around 3500'],
+        questionCorrectChoice: 2,
+        questionImage: 'policeman.jpg',
+        correctDetails: 'There are more than 3500 Car Manufacturers worldwide.'
+    },
+
+    //Question 6
+    {
+        questionText: 'How much does an average person drive in a year?',
+        questionChoices: ['About 13000 miles', 'About 15000 miles', 'About 20000 miles'],
+        questionCorrectChoice: 0,
+        questionImage: 'policeman.jpg',
+        correctDetails: 'About 13000 miles.'
+    },
+
+    //Question 7
+    {
+        questionText: 'How much does the average person in USA spend on gas each year?',
+        questionChoices: ['$3400', '$1000', '$1300'],
+        questionCorrectChoice: 2,
+        questionImage: 'policeman.jpg',
+        correctDetails: 'About $1300.'
+    },
+
+    //Question 8
+    {
+        questionText: 'What is the best selling car in the world?',
+        questionChoices: ['Volkswagen Beetle', 'Toyota Corolla', 'Ford Model T'],
+        questionCorrectChoice: 1,
+        questionImage: 'policeman.jpg',
+        correctDetails: 'Toyota Corolla sold 40000 cars worldwide.'
+    },
+
+    //Question 9
+    {
+        questionText: 'How long is the longest car in the world?',
+        questionChoices: ['10feet', '65feet', '100 feet'],
+        questionCorrectChoice: 2,
+        questionImage: 'policeman.jpg',
+        correctDetails: 'A 30.5 m (100 ft) long 26-wheeled limousine was designed by Jay Ohrberg of Burbank, California, USA.'
+    },
+
+    //Question 10
+    {
+        questionText: 'What is price of the cheapest car in the world?',
+        questionChoices: ['$1000', '$3000', '$2000'],
+        questionCorrectChoice: 2,
+        questionImage: 'policeman.jpg',
+        correctDetails: 'The Tata Nano is a city car manufactured in India with a price of US$2000 new.'
+    }
+];
+
+/*--- Step 2 - Defining functions ---*/
+
+
+/*--- Step 3 - Using functions ---*/
+$(document).ready(function () {
+    /*on click */
+});
